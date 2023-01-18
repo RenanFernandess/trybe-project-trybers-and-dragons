18ef3a09312ffd1134eb728495b11eae385cff8e
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 17:56:23 -0300

    :truck: chore: Remomeia o nome da pasta ArcheType para ArcheTypes

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,12 +1,10 @@
 import { EnergyType } from '../Energy';
 
 export default abstract class Archetype {
-  private _name: string;
   private _special: number;
   private _cost: number;
 
-  constructor(name: string) {
-    this._name = name;
+  constructor(private _name: string) {
     this._special = 0;
     this._cost = 0;
   }
@@ -16,7 +14,7 @@ export default abstract class Archetype {
   get cost(): number { return this._cost; }
   abstract get energyType(): EnergyType;
 
-  static createdArchetypeInstances(): number {
+  public static createdArchetypeInstances(): number {
     throw new Error('Not implemented');
   }
 }
