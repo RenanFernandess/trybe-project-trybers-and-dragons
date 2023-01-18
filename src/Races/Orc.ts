3eb05a2581360062bdb892397fea682ec5f07412
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 16:24:08 -0300

    :sparkles: feat: Implementa o metodo createdRacesInstances que retorna o numero de instancias criadas a partir da classe Orc

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -2,11 +2,17 @@ import Race from './Race';
 
 export default class Orc extends Race {
   private _maxLifePoints: number;
+  static _createdRacesInstaces = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 74;
+    Orc._createdRacesInstaces += 1;
   }
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+  
+  static createdRacesInstances(): number {
+    return Orc._createdRacesInstaces;
+  }
 }
