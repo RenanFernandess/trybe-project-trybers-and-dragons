b2f885d6757ebc0850cdbb2a0788bec8975e869b
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 16:20:40 -0300

    :sparkles: feat: Implementa o metodo createdRacesInstances que retorna o numero de instancias criadas a partir da classe Dwarf

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,11 +2,17 @@ import Race from './Race';
 
 export default class Dwarf extends Race {
   private _maxLifePoints: number;
+  static _createdRacesInstaces = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 80;
+    Dwarf._createdRacesInstaces += 1;
   }
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    return Dwarf._createdRacesInstaces;
+  }
 }
