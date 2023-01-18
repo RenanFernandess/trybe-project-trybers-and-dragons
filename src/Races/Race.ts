525b587ae9d49da3d4438127a050f2ceec956fbc
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 15:31:06 -0300

    :recycle: refactor: Adiona o get name e get dexterity

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,9 +1,11 @@
 export default abstract class Race {
   constructor(
     private _name: string,
-    private _dexterrity: number,
+    private _dexterity: number,
   ) {}
   
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
   abstract get maxLifePoints(): number;
 
   static createdRacesInstances(): number {
