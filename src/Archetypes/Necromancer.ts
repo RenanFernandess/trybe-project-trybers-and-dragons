327130e369a9e538c6a342d2beb41fb5ee5a2be3
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 18:10:23 -0300

    :sparkles: feat: Implementa a classe Necromancer que extende de Archetype

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default abstract class Necromancer extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    Necromancer._createdArchetypeInstances += 1;
+    this._energyType = 'mana';
+  }
+
+  get energyType(): EnergyType { return this._energyType; }
+
+  public static createdArchetypeInstances(): number {
+    return Necromancer._createdArchetypeInstances;
+  }
+}
