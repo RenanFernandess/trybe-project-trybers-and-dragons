fcf3f83e4e206c1ad9a13d0a07ca299d612d8bda
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 16:21:51 -0300

    :sparkles: feat: Implementa o metodo createdRacesInstances que retorna o numero de instancias criadas a partir da classe Elf

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -2,11 +2,17 @@ import Race from './Race';
 
 export default class Elf extends Race {
   private _maxLifePoints: number;
+  static _createdRacesInstaces = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 99;
+    Elf._createdRacesInstaces += 1;
   }
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances(): number {
+    return Elf._createdRacesInstaces;
+  }
 }
