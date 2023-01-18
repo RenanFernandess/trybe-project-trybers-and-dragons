6fb3f09ec0d7e84e3ec9123c24dcc5f7e3ade2b5
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 18:13:30 -0300

    :sparkles: feat: Implementa a classe Ranger que extende de Archetype

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default abstract class Ranger extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    Ranger._createdArchetypeInstances += 1;
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType { return this._energyType; }
+
+  public static createdArchetypeInstances(): number {
+    return Ranger._createdArchetypeInstances;
+  }
+}
