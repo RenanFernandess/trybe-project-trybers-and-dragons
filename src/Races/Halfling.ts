b132f069b2787ba9760212534750be90e3748d89
Renan Fernandes <renzinestuods@gmail.com>
2023-01-18 16:23:20 -0300

    :sparkles: feat: Implementa o metodo createdRacesInstances que retorna o numero de instancias criadas a partir da classe Halfling

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -2,11 +2,17 @@ import Race from './Race';
 
 export default class Halfling extends Race {
   private _maxLifePoints: number;
+  static _createdRacesInstaces = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 60;
+    Halfling._createdRacesInstaces += 1;
   }
 
   get maxLifePoints(): number { return this._maxLifePoints; }
+  
+  static createdRacesInstances(): number {
+    return Halfling._createdRacesInstaces;
+  }
 }
