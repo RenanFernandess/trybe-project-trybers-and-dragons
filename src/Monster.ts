16b44bb6bed2c2338e23ad5f18428cad30720b2a
Renan Fernandes <renzinestuods@gmail.com>
2023-01-19 20:34:43 -0300

    :boom: fix: tipa enemy como Fighter e SimpleFighter

--- src/Monster.ts
+++ src/Monster.ts
@@ -19,7 +19,7 @@ export default class Monster implements SimpleFighter {
     return this._lifePoints;
   }
 
-  public attack(enemy: Fighter): void {
+  public attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 }
