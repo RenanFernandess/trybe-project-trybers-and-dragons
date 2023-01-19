d6d5cb73ff2b6f149d53542f8c820e6df0808826
Renan Fernandes <renzinestuods@gmail.com>
2023-01-19 11:29:35 -0300

    :sparkles: feat: Implementa o metodo levelUp

--- src/Character.ts
+++ src/Character.ts
@@ -2,6 +2,7 @@ import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private _archetype: Archetype;
@@ -14,16 +15,16 @@ export default class Character implements Fighter {
   private _energy: Energy;
 
   constructor(name: string) {
-    this._dexterity = Math.floor(Math.random() * 10) + 1;
+    this._dexterity = getRandomInt(1, 10);
     this._race = new Elf(name, this._dexterity);
     this._archetype = new Mage(name);
     this._maxLifePoints = this._race.maxLifePoints;
     this._lifePoints = this._maxLifePoints;
-    this._strength = Math.floor(Math.random() * 10) + 1;
-    this._defense = Math.floor(Math.random() * 10) + 1;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
     this._energy = {
       type_: this._archetype.energyType,
-      amount: Math.floor(Math.random() * 10) + 1,
+      amount: getRandomInt(1, 10),
     };
   }
 
@@ -45,5 +46,17 @@ export default class Character implements Fighter {
 
   public attack(enemy: Fighter): void {
     enemy.receiveDamage(this._strength);
+  }
+
+  public levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
   }
 }
